a7b4c777680647c091c05f2a1dfb842fecb6943c
sapientiam-congnitio <95373163+sapientiam-congnitio@users.noreply.github.com>
2024-10-05 23:40:26 +0300

    update 3

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, request, jsonify
 from bs4 import BeautifulSoup
 import requests
 from get_articles import clean_articles
+import os
+
 
 
 app = Flask(__name__)
 
 
-@app.get("/")
+@app.get("/scrape")
 def response():
 
     url = request.args.get('url', default='https://www.piletilevi.ee/eng/tickets/koik/', type=str)
@@ -26,3 +28,8 @@ def response():
 
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
